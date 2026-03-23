00da4854c544768771c9860d6b3ca82dc4d77bc1
kjayasurya78 <kjayasurya78@zohomail.in>
2026-03-23 10:52:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy all resources"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
